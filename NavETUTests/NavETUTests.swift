bfea8d6ce5633a9b0be943752d9efb956f0b73c5
Ekaterina Ryzhova <rubicante@icloud.com>
2017-09-17 22:10:10 +0300

    Initial Commit

---
+++ NavETUTests/NavETUTests.swift
@@ -0,0 +1,36 @@
+//
+//  NavETUTests.swift
+//  NavETUTests
+//
+//  Created by Екатерина Рыжова on 17.09.17.
+//  Copyright © 2017 Екатерина Рыжова. All rights reserved.
+//
+
+import XCTest
+@testable import NavETU
+
+class NavETUTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
