89ee7f1446ffb784f7f75578147b84e1cc5dfea6
Unknown <rubicante@icloud.com>
2018-05-13 15:32:19 +0300

    add FacultiesVC, FacultyCell and content for RUS localization

---
+++ NavETU/Scenes/Home/Faculties/FacultiesDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  NavETU
+//
+//  Created by Екатерина Рыжова on 29.04.2018.
+//  Copyright © 2018 Екатерина Рыжова. All rights reserved.
+//
+
+import Foundation
---
+++ NavETU/View/FacultyCell.swift
@@ -0,0 +1,18 @@
+//
+//  FacultyCell.swift
+//  NavETU
+//
+//  Created by Екатерина Рыжова on 13.05.2018.
+//  Copyright © 2018 Екатерина Рыжова. All rights reserved.
+//
+
+import UIKit
+
+class FacultyCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
