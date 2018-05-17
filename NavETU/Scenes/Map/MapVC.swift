438c90f0d1be648292fc2704d98497661ce285fc
Unknown <rubicante@icloud.com>
2018-05-17 19:33:51 +0300

    >

--- NavETU/Scenes/Map/MapVC.swift
+++ NavETU/Scenes/Map/MapVC.swift
@@ -8,28 +8,80 @@
 
 import UIKit
 
-class MapVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        title = "Map.title".localized
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class MapVC: UIViewController, UIScrollViewDelegate {
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var floorButtons: UIStackView!
+    @IBOutlet var firstFloorButton: UIButton!
+    @IBOutlet var secondFloorButton: UIButton!
+    
+    @IBAction func tapFirstFloorButton(_ sender: UIButton) {
+        secondFloorButton.isSelected = false
+        imageView.image = #imageLiteral(resourceName: "floor1")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func tapSecondFloorButton(_ sender: UIButton) {
+        firstFloorButton.isSelected = false
+        imageView.image = #imageLiteral(resourceName: "floor2")
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.frame = view.frame
+        title = "Map.title".localized
+        imageView.image = #imageLiteral(resourceName: "floor1")
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.addSubview(imageView)
+        
+        scrollView.delegate = self
+        setZoomScale()
+//        setupGestureRecognizer()
+    }
 
+    override func viewWillLayoutSubviews() {
+        setZoomScale()
+    }
+    
+    func setZoomScale() {
+        let imageViewSize = imageView.bounds.size
+        let scrollViewSize = scrollView.bounds.size
+        let widthScale = scrollViewSize.width / imageViewSize.width
+        let heightScale = scrollViewSize.height / imageViewSize.height
+        
+        scrollView.minimumZoomScale = 0.1// min(widthScale, heightScale)
+        scrollView.zoomScale = 1.0
+    }
+
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        let imageViewSize = imageView.frame.size
+        let scrollViewSize = scrollView.bounds.size
+        
+        let verticalPadding = imageViewSize.height < scrollViewSize.height ?
+            (scrollViewSize.height - imageViewSize.height) / 2 : 0
+        let horizontalPadding = imageViewSize.width < scrollViewSize.width ?
+            (scrollViewSize.width - imageViewSize.width) / 2 : 0
+        
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding,
+                                               left: horizontalPadding,
+                                               bottom: verticalPadding,
+                                               right: horizontalPadding)
+    }
+    
+    func setupGestureRecognizer() {
+        let doubleTap = UITapGestureRecognizer(target: scrollView, action: "handleDoubleTap:")
+        doubleTap.numberOfTapsRequired = 2
+        scrollView.addGestureRecognizer(doubleTap)
+    }
+    
+    func handleDoubleTap(recognizer: UITapGestureRecognizer) {
+        if (scrollView.zoomScale > scrollView.minimumZoomScale) {
+            scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
+        } else {
+            scrollView.setZoomScale(scrollView.maximumZoomScale, animated: true)
+        }
+    }
 }
