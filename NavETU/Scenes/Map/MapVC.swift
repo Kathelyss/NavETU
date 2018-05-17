32b83dd6ec8f9bcfc8bc8e3cefda28eec67158ed
Unknown <rubicante@icloud.com>
2018-05-17 21:14:17 +0300

    fix map zoom

--- NavETU/Scenes/Map/MapVC.swift
+++ NavETU/Scenes/Map/MapVC.swift
@@ -27,18 +27,20 @@ class MapVC: UIViewController, UIScrollViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        scrollView.frame = view.frame
         title = "Map.title".localized
-        scrollView.contentSize = imageView.bounds.size
-        scrollView.addSubview(imageView)
-        setupFloorButtons()
         imageView.image = #imageLiteral(resourceName: "floor1")
+        scrollView.contentSize = imageView.frame.size
+        scrollView.minimumZoomScale = 0.3
+        scrollView.maximumZoomScale = 2
+        setupFloorButtons()
         firstFloorButton.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
-        scrollView.delegate = self
-        setZoomScale()
 //        setupGestureRecognizer()
     }
     
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+
     func setBackground(color1: UIColor, color2: UIColor, for button1: UIButton, and button2: UIButton) {
         button1.backgroundColor = color1
         button2.backgroundColor = color2
@@ -55,38 +57,18 @@ class MapVC: UIViewController, UIScrollViewDelegate {
         secondFloorButton.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
         setBackground(color1: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), color2: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), for: firstFloorButton, and: secondFloorButton)
     }
-    
-    override func viewWillLayoutSubviews() {
-        setZoomScale()
-    }
-    
-    func setZoomScale() {
-        let imageViewSize = imageView.bounds.size
-        let scrollViewSize = scrollView.bounds.size
-        let widthScale = scrollViewSize.width / imageViewSize.width
-        let heightScale = scrollViewSize.height / imageViewSize.height
-        
-        scrollView.minimumZoomScale = 0.1// min(widthScale, heightScale)
-        scrollView.zoomScale = 1.0
-    }
 
-    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
-        return imageView
-    }
-    
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
         let imageViewSize = imageView.frame.size
         let scrollViewSize = scrollView.bounds.size
-        
+
         let verticalPadding = imageViewSize.height < scrollViewSize.height ?
             (scrollViewSize.height - imageViewSize.height) / 2 : 0
         let horizontalPadding = imageViewSize.width < scrollViewSize.width ?
             (scrollViewSize.width - imageViewSize.width) / 2 : 0
-        
-        scrollView.contentInset = UIEdgeInsets(top: verticalPadding,
-                                               left: horizontalPadding,
-                                               bottom: verticalPadding,
-                                               right: horizontalPadding)
+
+        scrollView.contentInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding,
+                                               bottom: verticalPadding, right: horizontalPadding)
     }
     
     func setupGestureRecognizer() {
