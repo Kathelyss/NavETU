2e9bbc773dc4a5988de03385cd303fcae20cd12c
Unknown <rubicante@icloud.com>
2018-05-17 19:48:02 +0300

    setup floor buttons

--- NavETU/Scenes/Map/MapVC.swift
+++ NavETU/Scenes/Map/MapVC.swift
@@ -16,12 +16,12 @@ class MapVC: UIViewController, UIScrollViewDelegate {
     @IBOutlet var secondFloorButton: UIButton!
     
     @IBAction func tapFirstFloorButton(_ sender: UIButton) {
-        secondFloorButton.isSelected = false
+        setBackground(color1: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), color2: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), for: firstFloorButton, and: secondFloorButton)
         imageView.image = #imageLiteral(resourceName: "floor1")
     }
     
     @IBAction func tapSecondFloorButton(_ sender: UIButton) {
-        firstFloorButton.isSelected = false
+        setBackground(color1: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), color2: #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), for: firstFloorButton, and: secondFloorButton)
         imageView.image = #imageLiteral(resourceName: "floor2")
     }
     
@@ -29,15 +29,33 @@ class MapVC: UIViewController, UIScrollViewDelegate {
         super.viewDidLoad()
         scrollView.frame = view.frame
         title = "Map.title".localized
-        imageView.image = #imageLiteral(resourceName: "floor1")
         scrollView.contentSize = imageView.bounds.size
         scrollView.addSubview(imageView)
-        
+        setupFloorButtons()
+        imageView.image = #imageLiteral(resourceName: "floor1")
+        firstFloorButton.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
         scrollView.delegate = self
         setZoomScale()
 //        setupGestureRecognizer()
     }
+    
+    func setBackground(color1: UIColor, color2: UIColor, for button1: UIButton, and button2: UIButton) {
+        button1.backgroundColor = color1
+        button2.backgroundColor = color2
+    }
 
+    func setupFloorButtons() {
+        firstFloorButton.layer.masksToBounds = true
+        firstFloorButton.layer.cornerRadius = 5
+        firstFloorButton.layer.borderWidth = 1
+        firstFloorButton.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        secondFloorButton.layer.masksToBounds = true
+        secondFloorButton.layer.cornerRadius = 5
+        secondFloorButton.layer.borderWidth = 1
+        secondFloorButton.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        setBackground(color1: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), color2: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1), for: firstFloorButton, and: secondFloorButton)
+    }
+    
     override func viewWillLayoutSubviews() {
         setZoomScale()
     }
